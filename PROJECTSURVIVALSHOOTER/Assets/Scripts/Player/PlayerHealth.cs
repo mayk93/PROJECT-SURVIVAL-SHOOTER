6a8e3c32c6f2754a83fb9ca089ac67f03978901c
mayk93 <mihai.mandrescu@gmail.com>
2015-03-17 23:56:19 +0200

    Part 8
    Part 8 completed.

--- PROJECTSURVIVALSHOOTER/Assets/Scripts/Player/PlayerHealth.cs
+++ PROJECTSURVIVALSHOOTER/Assets/Scripts/Player/PlayerHealth.cs
@@ -16,7 +16,7 @@ public class PlayerHealth : MonoBehaviour
     Animator anim;
     AudioSource playerAudio;
     PlayerMovement playerMovement; // We make a referennce to a script. We disable it after death
-    //PlayerShooting playerShooting;
+    PlayerShooting playerShooting;
     bool isDead;
     bool damaged;
 
@@ -26,7 +26,7 @@ public class PlayerHealth : MonoBehaviour
         anim = GetComponent <Animator> ();
         playerAudio = GetComponent <AudioSource> ();
         playerMovement = GetComponent <PlayerMovement> (); 
-        //playerShooting = GetComponentInChildren <PlayerShooting> ();
+        playerShooting = GetComponentInChildren <PlayerShooting> ();
         currentHealth = startingHealth;
     }
 
@@ -67,7 +67,7 @@ public class PlayerHealth : MonoBehaviour
     {
         isDead = true;
 
-        //playerShooting.DisableEffects ();
+        playerShooting.DisableEffects ();
 
         anim.SetTrigger ("Die");
 
@@ -75,7 +75,7 @@ public class PlayerHealth : MonoBehaviour
         playerAudio.Play ();
 
         playerMovement.enabled = false;
-        //playerShooting.enabled = false;
+        playerShooting.enabled = false;
     }
 
 
